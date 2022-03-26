7f6178211b127710a38a784adc97ba823ed29ef0
taozhenkai <zk_tao@163.com>
2022-03-26 23:53:17 +0800

    :sparkles: 2.8.4 特征进阶

---
+++ advance-trait-demo/src/main.rs
@@ -0,0 +1,49 @@
+// #![allow(unused)]
+// trait Pilot {
+//     fn fly(&self);
+// }
+
+// trait Wizard {
+//     fn fly(&self);
+// }
+
+// struct Human;
+
+// impl Pilot for Human {
+//     fn fly(&self) {
+//         println!("This is your captain speaking.");
+//     }
+// }
+
+// impl Wizard for Human {
+//     fn fly(&self) {
+//         println!("Up!");
+//     }
+// }
+
+// impl Human {
+//     fn fly(&self) {
+//         println!("*waving arms furiously*");
+//     }
+// }
+// fn main() {
+//     let person = Human;
+//     Pilot::fly(&person); // 调用Pilot特征上的方法
+//     Wizard::fly(&person); // 调用Wizard特征上的方法
+//     person.fly(); // 调用Human类型自身的方法
+// }
+
+use std::fmt;
+
+struct Wrapper(Vec<String>);
+
+impl fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[{}]", self.0.join(", "))
+    }
+}
+
+fn main() {
+    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+    println!("w = {}", w);
+}
