2731f79ce2f3565f65c5451fe989419ed4e19c82
taozhenkai <zk_tao@163.com>
2022-05-19 10:37:42 +0800

    :sparkles: 3.3.1 newtype demo

---
+++ newtype-demo/src/main.rs
@@ -0,0 +1,25 @@
+// use std::fmt;
+
+// struct Wrapper(Vec<String>);
+
+// impl fmt::Display for Wrapper {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         write!(f, "[{}]", self.0.join(", "))
+//     }
+// }
+
+// fn main() {
+//     let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+//     println!("w = {}", w);
+// }
+
+struct Meters(u32);
+
+fn main() {
+    let i: u32 = 2;
+    assert_eq!(i.pow(2), 4);
+
+    let n = Meters(i);
+    // 下面的代码将报错，因为`Meters`类型上没有`pow`方法
+    // assert_eq!(n.pow(2), 4);
+}
