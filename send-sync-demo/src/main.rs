19ee69adf68a7a60443f5ab46538b2e109d6fe6a
taozhenkai <zk_tao@163.com>
2022-07-04 22:28:52 +0800

    :sparkles: 3.6.6 sned&sync demo

---
+++ send-sync-demo/src/main.rs
@@ -0,0 +1,24 @@
+// use std::thread;
+// use std::rc::Rc;
+// fn main() {
+//     let v = Rc::new(5);
+//     let t = thread::spawn(move || {
+//         println!("{}",v);
+//     });
+
+//     t.join().unwrap();
+// }
+
+use std::thread;
+
+#[derive(Debug)]
+struct MyBox(*mut u8);
+unsafe impl Send for MyBox {}
+fn main() {
+    let p = MyBox(5 as *mut u8);
+    let t = thread::spawn(move || {
+        println!("{:?}", p);
+    });
+
+    t.join().unwrap();
+}
